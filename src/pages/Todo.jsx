f3eef236e54cc50b1a8f446444e0b5df8e7928d3
sujin <soojin9877@naver.com>
2022-12-15 10:56:39 +0900

    Modify: h1 수정

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -31,7 +31,7 @@ const Todo = () => {
         <>
             {!token && <Navigate to="/" replace={true} />}
             <main>
-                <h1>TO</h1>
+                <h1>TODO LIST</h1>
                 <section>
                     <input
                         type="text"
